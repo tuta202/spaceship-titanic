5f301764392b8843cbdf2606557132f2ec67c774
tuchan2002 <trananhtu171002@gmail.com>
2025-03-06 22:42:16 +0700

    push

--- classifier.py
+++ classifier.py
@@ -17,7 +17,7 @@ n_cols = 3
 fig, ax = plt.subplots(n_rows, n_cols, figsize=(n_cols*3.5, n_rows*3.5))
 for r in range(0, n_rows):
   for c in range(0, n_cols):
-    i = r*n_rows + c
+    i = r*n_cols + c
     if i < len(cols):
       ax_i = ax[r, c]
       sns.countplot(data=data, x=cols[i], hue=target, ax=ax_i)
