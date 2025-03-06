bd55106ad42ef6e87a0b7de053d207073e57e53d
tuchan2002 <trananhtu171002@gmail.com>
2025-03-06 21:01:01 +0700

    remove comment

--- classifier.py
+++ classifier.py
@@ -3,28 +3,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 data = pd.read_csv("data/train.csv")
 x_test = pd.read_csv("data/test.csv", index_col="PassengerId")
-
-# profile = ProfileReport(data, title="Titanic", explorative=True)
-# profile.to_file("titanic.html")
 
 target = "Transported"
 
 data.set_index("PassengerId", inplace=True)
-
-# sns.histplot(data=data["Age"])
-# sns.kdeplot(data=data["Age"])
-# sns.displot(data=data, x="Age", col="Transported")
-# sns.barplot(data=data, x="Transported", y="RoomService", estimator=np.mean)
-
-# fg_instance = sns.FacetGrid(data=data, col="HomePlanet", hue="Transported")
-# fg_instance.map(sns.scatterplot, "RoomService", "Spa", s=50, edgecolor="b", alpha=0.5)
-# fg_instance.add_legend()
-
-# sns.scatterplot(data=data, x="RoomService", y="Spa", hue="HomePlanet")
-
-# sns.pairplot(data=data, hue="HomePlanet")
-
-plt.show()
